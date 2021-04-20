632ce10c79be465054897c22737adc8a652a7f39
Yoshi <yoshis1057@gmail.com>
2021-04-20 02:31:57 +0000

    finish basic in Basics

--- src/main.go
+++ src/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"app/basic"
+	"app/basics"
 	"fmt"
 )
 
 func main() {
+	// 参考url: https://go-tour-jp.appspot.com/basics/11
+	checkBasic()
+}
+
+func checkBasic() {
+	// 1
+	fmt.Println(basics.Add(10, 20))
+	// 2
+	a := "Sing"
+	b := "Song"
+	a, b = basics.Swap(a, b)
+	fmt.Println(a, b)
+	// 3
+	basics.Variables()
+	// 4
+	basics.VariablesWithInitializers()
+	// 5
+	basics.ShortVariableDeclarations()
+	// 6
+	basics.TypeConversions()
+	// 7
+	basics.Constants()
 }
