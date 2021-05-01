6758766a5ad0fe9331efb2d67eebdfd5d1a92510
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-05-01 03:10:06 +0000

    fix bubble sort

--- src/test/sorting_algorithm/bubblesort.go
+++ src/test/sorting_algorithm/bubblesort.go
@@ -2,11 +2,11 @@ package sorting_algorithm
 
 func BubbleSort(data []int) {
 	for loop_num := 0; loop_num < len(data); loop_num++ {
-		for index := 0; index < len(data)-1-loop_num; index++ {
-			if data[index] > data[index+1] {
+		for index := len(data) - 1; index > loop_num; index-- {
+			if data[index] > data[index-1] {
 				tmp := data[index]
-				data[index] = data[index+1]
-				data[index+1] = tmp
+				data[index] = data[index-1]
+				data[index-1] = tmp
 			}
 		}
 	}
