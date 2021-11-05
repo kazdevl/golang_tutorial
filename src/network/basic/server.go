d9421dee7a3341bc663160a6938058d5b57ee3a3
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-11-05 14:38:52 +0900

    test send and recieve sturct that has json tag

--- src/network/basic/server.go
+++ src/network/basic/server.go
@@ -1,6 +1,7 @@
 package basic
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 )
@@ -22,4 +23,27 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, `{"content": "Hello World"}`)
+}
+
+type Sample struct {
+	A string `json:"a"`
+	B int    `json:"b"`
+}
+
+func JsonStructThrowHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8s")
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, `{"error": "request method should be Post"}`)
+		return
+	}
+	data := []Sample{{A: "sample1", B: 1}, {A: "sample2", B: 2}}
+	msg, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, `{"error": "when struct convert to json"}`)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(msg)
 }
--- src/network/basic/server_test.go
+++ src/network/basic/server_test.go
@@ -2,12 +2,15 @@ package basic_test
 
 import (
 	"app/network/basic"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
-func Test_Handler(t *testing.T) {
+func Test_HelloHandler(t *testing.T) {
 	type input struct {
 		req *http.Request
 	}
@@ -60,6 +63,60 @@ func Test_Handler(t *testing.T) {
 
 			if got := got.Body.String(); test.want.respBody != got {
 				t.Errorf("want %s, but %s", test.want.respBody, got)
+			}
+		})
+	}
+}
+
+func Test_JsonHandler(t *testing.T) {
+	type input struct {
+		req *http.Request
+	}
+	type want struct {
+		statusCode  int
+		contentType string
+		respBody    []basic.Sample
+	}
+	tests := []struct {
+		name  string
+		input input
+		want  want
+	}{
+		{
+			name: "success pattern",
+			input: input{
+				req: httptest.NewRequest(http.MethodPost, "http://dummy.url.com", nil),
+			},
+			want: want{
+				statusCode:  http.StatusOK,
+				contentType: "application/json; charset=UTF-8s",
+				respBody:    []basic.Sample{{A: "sample1", B: 1}, {A: "sample2", B: 2}},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := httptest.NewRecorder()
+			basic.JsonStructThrowHandler(got, test.input.req)
+
+			if test.want.statusCode != got.Code {
+				t.Errorf("want %d, but %d", test.want.statusCode, got.Code)
+			}
+
+			if contentType := got.Result().Header["Content-Type"]; test.want.contentType != contentType[0] {
+				t.Errorf("want %s, but %s", test.want.contentType, contentType[0])
+			}
+
+			body, err := io.ReadAll(got.Result().Body)
+			if err != nil {
+				t.Error(err)
+			}
+			defer got.Result().Body.Close()
+			result := []basic.Sample{}
+			json.Unmarshal(body, &result) //ポインタにする必要がある
+			if !reflect.DeepEqual(result, test.want.respBody) {
+				t.Errorf("want %v, but %v", result, test.want.respBody)
 			}
 		})
 	}
