0c366f0508c7cc43058fff3ae54092f509daf884
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-01 00:11:15 +0900

    update

--- src/network/http/protocol/main.go
+++ src/network/http/protocol/main.go
@@ -5,6 +5,7 @@ import "net/http"
 func main() {
 	// TCP
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("/")))
+	mux.Handle("/", http.FileServer(http.Dir("./")))
+	// http1.1
 	http.ListenAndServe(":8080", mux)
 }
