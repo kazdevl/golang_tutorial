7f920509129f006c5575480d28a90527804945f6
kazu <39262724+kazdevl@users.noreply.github.com>
2022-01-14 17:40:09 +0900

    update

--- src/linter/each/13/main.go
+++ src/linter/each/13/main.go
@@ -5,303 +5,603 @@ import "fmt"
 func main() {
     
     const SAMPLE0 = "0"
-    fmt.Println(SAMPLE0)
+    if 0 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE0")
+    }
+    fmt.Println("奇数: SAMPLE0")
     
     const SAMPLE1 = "1"
-    fmt.Println(SAMPLE1)
+    if 1 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE1")
+    }
+    fmt.Println("奇数: SAMPLE1")
     
     const SAMPLE2 = "2"
-    fmt.Println(SAMPLE2)
+    if 2 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE2")
+    }
+    fmt.Println("奇数: SAMPLE2")
     
     const SAMPLE3 = "3"
-    fmt.Println(SAMPLE3)
+    if 3 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE3")
+    }
+    fmt.Println("奇数: SAMPLE3")
     
     const SAMPLE4 = "4"
-    fmt.Println(SAMPLE4)
+    if 4 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE4")
+    }
+    fmt.Println("奇数: SAMPLE4")
     
     const SAMPLE5 = "5"
-    fmt.Println(SAMPLE5)
+    if 5 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE5")
+    }
+    fmt.Println("奇数: SAMPLE5")
     
     const SAMPLE6 = "6"
-    fmt.Println(SAMPLE6)
+    if 6 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE6")
+    }
+    fmt.Println("奇数: SAMPLE6")
     
     const SAMPLE7 = "7"
-    fmt.Println(SAMPLE7)
+    if 7 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE7")
+    }
+    fmt.Println("奇数: SAMPLE7")
     
     const SAMPLE8 = "8"
-    fmt.Println(SAMPLE8)
+    if 8 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE8")
+    }
+    fmt.Println("奇数: SAMPLE8")
     
     const SAMPLE9 = "9"
-    fmt.Println(SAMPLE9)
+    if 9 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE9")
+    }
+    fmt.Println("奇数: SAMPLE9")
     
     const SAMPLE10 = "10"
-    fmt.Println(SAMPLE10)
+    if 10 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE10")
+    }
+    fmt.Println("奇数: SAMPLE10")
     
     const SAMPLE11 = "11"
-    fmt.Println(SAMPLE11)
+    if 11 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE11")
+    }
+    fmt.Println("奇数: SAMPLE11")
     
     const SAMPLE12 = "12"
-    fmt.Println(SAMPLE12)
+    if 12 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE12")
+    }
+    fmt.Println("奇数: SAMPLE12")
     
     const SAMPLE13 = "13"
-    fmt.Println(SAMPLE13)
+    if 13 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE13")
+    }
+    fmt.Println("奇数: SAMPLE13")
     
     const SAMPLE14 = "14"
-    fmt.Println(SAMPLE14)
+    if 14 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE14")
+    }
+    fmt.Println("奇数: SAMPLE14")
     
     const SAMPLE15 = "15"
-    fmt.Println(SAMPLE15)
+    if 15 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE15")
+    }
+    fmt.Println("奇数: SAMPLE15")
     
     const SAMPLE16 = "16"
-    fmt.Println(SAMPLE16)
+    if 16 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE16")
+    }
+    fmt.Println("奇数: SAMPLE16")
     
     const SAMPLE17 = "17"
-    fmt.Println(SAMPLE17)
+    if 17 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE17")
+    }
+    fmt.Println("奇数: SAMPLE17")
     
     const SAMPLE18 = "18"
-    fmt.Println(SAMPLE18)
+    if 18 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE18")
+    }
+    fmt.Println("奇数: SAMPLE18")
     
     const SAMPLE19 = "19"
-    fmt.Println(SAMPLE19)
+    if 19 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE19")
+    }
+    fmt.Println("奇数: SAMPLE19")
     
     const SAMPLE20 = "20"
-    fmt.Println(SAMPLE20)
+    if 20 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE20")
+    }
+    fmt.Println("奇数: SAMPLE20")
     
     const SAMPLE21 = "21"
-    fmt.Println(SAMPLE21)
+    if 21 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE21")
+    }
+    fmt.Println("奇数: SAMPLE21")
     
     const SAMPLE22 = "22"
-    fmt.Println(SAMPLE22)
+    if 22 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE22")
+    }
+    fmt.Println("奇数: SAMPLE22")
     
     const SAMPLE23 = "23"
-    fmt.Println(SAMPLE23)
+    if 23 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE23")
+    }
+    fmt.Println("奇数: SAMPLE23")
     
     const SAMPLE24 = "24"
-    fmt.Println(SAMPLE24)
+    if 24 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE24")
+    }
+    fmt.Println("奇数: SAMPLE24")
     
     const SAMPLE25 = "25"
-    fmt.Println(SAMPLE25)
+    if 25 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE25")
+    }
+    fmt.Println("奇数: SAMPLE25")
     
     const SAMPLE26 = "26"
-    fmt.Println(SAMPLE26)
+    if 26 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE26")
+    }
+    fmt.Println("奇数: SAMPLE26")
     
     const SAMPLE27 = "27"
-    fmt.Println(SAMPLE27)
+    if 27 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE27")
+    }
+    fmt.Println("奇数: SAMPLE27")
     
     const SAMPLE28 = "28"
-    fmt.Println(SAMPLE28)
+    if 28 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE28")
+    }
+    fmt.Println("奇数: SAMPLE28")
     
     const SAMPLE29 = "29"
-    fmt.Println(SAMPLE29)
+    if 29 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE29")
+    }
+    fmt.Println("奇数: SAMPLE29")
     
     const SAMPLE30 = "30"
-    fmt.Println(SAMPLE30)
+    if 30 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE30")
+    }
+    fmt.Println("奇数: SAMPLE30")
     
     const SAMPLE31 = "31"
-    fmt.Println(SAMPLE31)
+    if 31 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE31")
+    }
+    fmt.Println("奇数: SAMPLE31")
     
     const SAMPLE32 = "32"
-    fmt.Println(SAMPLE32)
+    if 32 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE32")
+    }
+    fmt.Println("奇数: SAMPLE32")
     
     const SAMPLE33 = "33"
-    fmt.Println(SAMPLE33)
+    if 33 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE33")
+    }
+    fmt.Println("奇数: SAMPLE33")
     
     const SAMPLE34 = "34"
-    fmt.Println(SAMPLE34)
+    if 34 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE34")
+    }
+    fmt.Println("奇数: SAMPLE34")
     
     const SAMPLE35 = "35"
-    fmt.Println(SAMPLE35)
+    if 35 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE35")
+    }
+    fmt.Println("奇数: SAMPLE35")
     
     const SAMPLE36 = "36"
-    fmt.Println(SAMPLE36)
+    if 36 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE36")
+    }
+    fmt.Println("奇数: SAMPLE36")
     
     const SAMPLE37 = "37"
-    fmt.Println(SAMPLE37)
+    if 37 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE37")
+    }
+    fmt.Println("奇数: SAMPLE37")
     
     const SAMPLE38 = "38"
-    fmt.Println(SAMPLE38)
+    if 38 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE38")
+    }
+    fmt.Println("奇数: SAMPLE38")
     
     const SAMPLE39 = "39"
-    fmt.Println(SAMPLE39)
+    if 39 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE39")
+    }
+    fmt.Println("奇数: SAMPLE39")
     
     const SAMPLE40 = "40"
-    fmt.Println(SAMPLE40)
+    if 40 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE40")
+    }
+    fmt.Println("奇数: SAMPLE40")
     
     const SAMPLE41 = "41"
-    fmt.Println(SAMPLE41)
+    if 41 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE41")
+    }
+    fmt.Println("奇数: SAMPLE41")
     
     const SAMPLE42 = "42"
-    fmt.Println(SAMPLE42)
+    if 42 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE42")
+    }
+    fmt.Println("奇数: SAMPLE42")
     
     const SAMPLE43 = "43"
-    fmt.Println(SAMPLE43)
+    if 43 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE43")
+    }
+    fmt.Println("奇数: SAMPLE43")
     
     const SAMPLE44 = "44"
-    fmt.Println(SAMPLE44)
+    if 44 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE44")
+    }
+    fmt.Println("奇数: SAMPLE44")
     
     const SAMPLE45 = "45"
-    fmt.Println(SAMPLE45)
+    if 45 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE45")
+    }
+    fmt.Println("奇数: SAMPLE45")
     
     const SAMPLE46 = "46"
-    fmt.Println(SAMPLE46)
+    if 46 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE46")
+    }
+    fmt.Println("奇数: SAMPLE46")
     
     const SAMPLE47 = "47"
-    fmt.Println(SAMPLE47)
+    if 47 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE47")
+    }
+    fmt.Println("奇数: SAMPLE47")
     
     const SAMPLE48 = "48"
-    fmt.Println(SAMPLE48)
+    if 48 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE48")
+    }
+    fmt.Println("奇数: SAMPLE48")
     
     const SAMPLE49 = "49"
-    fmt.Println(SAMPLE49)
+    if 49 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE49")
+    }
+    fmt.Println("奇数: SAMPLE49")
     
     const SAMPLE50 = "50"
-    fmt.Println(SAMPLE50)
+    if 50 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE50")
+    }
+    fmt.Println("奇数: SAMPLE50")
     
     const SAMPLE51 = "51"
-    fmt.Println(SAMPLE51)
+    if 51 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE51")
+    }
+    fmt.Println("奇数: SAMPLE51")
     
     const SAMPLE52 = "52"
-    fmt.Println(SAMPLE52)
+    if 52 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE52")
+    }
+    fmt.Println("奇数: SAMPLE52")
     
     const SAMPLE53 = "53"
-    fmt.Println(SAMPLE53)
+    if 53 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE53")
+    }
+    fmt.Println("奇数: SAMPLE53")
     
     const SAMPLE54 = "54"
-    fmt.Println(SAMPLE54)
+    if 54 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE54")
+    }
+    fmt.Println("奇数: SAMPLE54")
     
     const SAMPLE55 = "55"
-    fmt.Println(SAMPLE55)
+    if 55 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE55")
+    }
+    fmt.Println("奇数: SAMPLE55")
     
     const SAMPLE56 = "56"
-    fmt.Println(SAMPLE56)
+    if 56 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE56")
+    }
+    fmt.Println("奇数: SAMPLE56")
     
     const SAMPLE57 = "57"
-    fmt.Println(SAMPLE57)
+    if 57 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE57")
+    }
+    fmt.Println("奇数: SAMPLE57")
     
     const SAMPLE58 = "58"
-    fmt.Println(SAMPLE58)
+    if 58 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE58")
+    }
+    fmt.Println("奇数: SAMPLE58")
     
     const SAMPLE59 = "59"
-    fmt.Println(SAMPLE59)
+    if 59 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE59")
+    }
+    fmt.Println("奇数: SAMPLE59")
     
     const SAMPLE60 = "60"
-    fmt.Println(SAMPLE60)
+    if 60 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE60")
+    }
+    fmt.Println("奇数: SAMPLE60")
     
     const SAMPLE61 = "61"
-    fmt.Println(SAMPLE61)
+    if 61 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE61")
+    }
+    fmt.Println("奇数: SAMPLE61")
     
     const SAMPLE62 = "62"
-    fmt.Println(SAMPLE62)
+    if 62 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE62")
+    }
+    fmt.Println("奇数: SAMPLE62")
     
     const SAMPLE63 = "63"
-    fmt.Println(SAMPLE63)
+    if 63 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE63")
+    }
+    fmt.Println("奇数: SAMPLE63")
     
     const SAMPLE64 = "64"
-    fmt.Println(SAMPLE64)
+    if 64 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE64")
+    }
+    fmt.Println("奇数: SAMPLE64")
     
     const SAMPLE65 = "65"
-    fmt.Println(SAMPLE65)
+    if 65 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE65")
+    }
+    fmt.Println("奇数: SAMPLE65")
     
     const SAMPLE66 = "66"
-    fmt.Println(SAMPLE66)
+    if 66 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE66")
+    }
+    fmt.Println("奇数: SAMPLE66")
     
     const SAMPLE67 = "67"
-    fmt.Println(SAMPLE67)
+    if 67 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE67")
+    }
+    fmt.Println("奇数: SAMPLE67")
     
     const SAMPLE68 = "68"
-    fmt.Println(SAMPLE68)
+    if 68 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE68")
+    }
+    fmt.Println("奇数: SAMPLE68")
     
     const SAMPLE69 = "69"
-    fmt.Println(SAMPLE69)
+    if 69 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE69")
+    }
+    fmt.Println("奇数: SAMPLE69")
     
     const SAMPLE70 = "70"
-    fmt.Println(SAMPLE70)
+    if 70 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE70")
+    }
+    fmt.Println("奇数: SAMPLE70")
     
     const SAMPLE71 = "71"
-    fmt.Println(SAMPLE71)
+    if 71 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE71")
+    }
+    fmt.Println("奇数: SAMPLE71")
     
     const SAMPLE72 = "72"
-    fmt.Println(SAMPLE72)
+    if 72 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE72")
+    }
+    fmt.Println("奇数: SAMPLE72")
     
     const SAMPLE73 = "73"
-    fmt.Println(SAMPLE73)
+    if 73 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE73")
+    }
+    fmt.Println("奇数: SAMPLE73")
     
     const SAMPLE74 = "74"
-    fmt.Println(SAMPLE74)
+    if 74 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE74")
+    }
+    fmt.Println("奇数: SAMPLE74")
     
     const SAMPLE75 = "75"
-    fmt.Println(SAMPLE75)
+    if 75 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE75")
+    }
+    fmt.Println("奇数: SAMPLE75")
     
     const SAMPLE76 = "76"
-    fmt.Println(SAMPLE76)
+    if 76 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE76")
+    }
+    fmt.Println("奇数: SAMPLE76")
     
     const SAMPLE77 = "77"
-    fmt.Println(SAMPLE77)
+    if 77 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE77")
+    }
+    fmt.Println("奇数: SAMPLE77")
     
     const SAMPLE78 = "78"
-    fmt.Println(SAMPLE78)
+    if 78 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE78")
+    }
+    fmt.Println("奇数: SAMPLE78")
     
     const SAMPLE79 = "79"
-    fmt.Println(SAMPLE79)
+    if 79 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE79")
+    }
+    fmt.Println("奇数: SAMPLE79")
     
     const SAMPLE80 = "80"
-    fmt.Println(SAMPLE80)
+    if 80 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE80")
+    }
+    fmt.Println("奇数: SAMPLE80")
     
     const SAMPLE81 = "81"
-    fmt.Println(SAMPLE81)
+    if 81 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE81")
+    }
+    fmt.Println("奇数: SAMPLE81")
     
     const SAMPLE82 = "82"
-    fmt.Println(SAMPLE82)
+    if 82 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE82")
+    }
+    fmt.Println("奇数: SAMPLE82")
     
     const SAMPLE83 = "83"
-    fmt.Println(SAMPLE83)
+    if 83 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE83")
+    }
+    fmt.Println("奇数: SAMPLE83")
     
     const SAMPLE84 = "84"
-    fmt.Println(SAMPLE84)
+    if 84 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE84")
+    }
+    fmt.Println("奇数: SAMPLE84")
     
     const SAMPLE85 = "85"
-    fmt.Println(SAMPLE85)
+    if 85 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE85")
+    }
+    fmt.Println("奇数: SAMPLE85")
     
     const SAMPLE86 = "86"
-    fmt.Println(SAMPLE86)
+    if 86 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE86")
+    }
+    fmt.Println("奇数: SAMPLE86")
     
     const SAMPLE87 = "87"
-    fmt.Println(SAMPLE87)
+    if 87 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE87")
+    }
+    fmt.Println("奇数: SAMPLE87")
     
     const SAMPLE88 = "88"
-    fmt.Println(SAMPLE88)
+    if 88 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE88")
+    }
+    fmt.Println("奇数: SAMPLE88")
     
     const SAMPLE89 = "89"
-    fmt.Println(SAMPLE89)
+    if 89 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE89")
+    }
+    fmt.Println("奇数: SAMPLE89")
     
     const SAMPLE90 = "90"
-    fmt.Println(SAMPLE90)
+    if 90 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE90")
+    }
+    fmt.Println("奇数: SAMPLE90")
     
     const SAMPLE91 = "91"
-    fmt.Println(SAMPLE91)
+    if 91 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE91")
+    }
+    fmt.Println("奇数: SAMPLE91")
     
     const SAMPLE92 = "92"
-    fmt.Println(SAMPLE92)
+    if 92 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE92")
+    }
+    fmt.Println("奇数: SAMPLE92")
     
     const SAMPLE93 = "93"
-    fmt.Println(SAMPLE93)
+    if 93 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE93")
+    }
+    fmt.Println("奇数: SAMPLE93")
     
     const SAMPLE94 = "94"
-    fmt.Println(SAMPLE94)
+    if 94 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE94")
+    }
+    fmt.Println("奇数: SAMPLE94")
     
     const SAMPLE95 = "95"
-    fmt.Println(SAMPLE95)
+    if 95 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE95")
+    }
+    fmt.Println("奇数: SAMPLE95")
     
     const SAMPLE96 = "96"
-    fmt.Println(SAMPLE96)
+    if 96 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE96")
+    }
+    fmt.Println("奇数: SAMPLE96")
     
     const SAMPLE97 = "97"
-    fmt.Println(SAMPLE97)
+    if 97 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE97")
+    }
+    fmt.Println("奇数: SAMPLE97")
     
     const SAMPLE98 = "98"
-    fmt.Println(SAMPLE98)
+    if 98 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE98")
+    }
+    fmt.Println("奇数: SAMPLE98")
     
     const SAMPLE99 = "99"
-    fmt.Println(SAMPLE99)
+    if 99 % 2 == 0 {
+        fmt.Println("偶数: SAMPLE99")
+    }
+    fmt.Println("奇数: SAMPLE99")
     
 }
