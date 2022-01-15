92d0da7b8823a51962a7588ea4038be2d0771a1c
kazu <39262724+kazdevl@users.noreply.github.com>
2022-01-15 21:30:28 +0900

    impl ranking with redis

--- src/redis/go-redis/ranking/main.go
+++ src/redis/go-redis/ranking/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/hashicorp/go.net/context"
 )
 
 type Group struct {
@@ -23,35 +23,73 @@ type RedisSorter struct {
 }
 
 func main() {
+	gset := []Group{
+		{
+			ID: "a",
+			Users: []Rankig{
+				{"Hoge1", 100},
+				{"Hoge2", 10},
+				{"Hoge3", 50},
+			},
+		},
+		{
+			ID: "b",
+			Users: []Rankig{
+				{"Hoge101", 300},
+				{"Hoge201", 200},
+				{"Hoge301", 500},
+			},
+		},
+	}
 
+	sorter := NewRedisSorter()
+	for _, g := range gset {
+		sorter.Add(context.Background(), g.ID, g.Users)
+	}
+
+	// get top of group "a"
+	topUser, err := sorter.GetTop(context.Background(), gset[0].ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(topUser)
+
+	// get "Hoge101"'s Rank
+	targetUserRank, err := sorter.GetRank(context.Background(), gset[1].ID, "Hoge101")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(targetUserRank)
 }
 
 func NewRedisSorter() *RedisSorter {
 	return &RedisSorter{redisClient: redis.NewClient(&redis.Options{
-		Addr: "http://localhost:6380",
+		Addr: ":6380",
 	})}
 }
 
-func (r *RedisSorter) Add(ctx context.Context, key string, data Rankig) error {
-	cmd := r.redisClient.ZAdd(ctx, key, &redis.Z{Score: float64(data.Score), Member: data.User})
+func (r *RedisSorter) Add(ctx context.Context, key string, data []Rankig) error {
+	members := make([]*redis.Z, 0, len(data))
+	for _, d := range data {
+		members = append(members, &redis.Z{Score: float64(d.Score), Member: d.User})
+	}
+	cmd := r.redisClient.ZAdd(ctx, key, members...)
 	return cmd.Err()
 }
 
-func (r *RedisSorter) GetRank(ctx context.Context, key, id string) (int64, error) {
-	cmd := r.redisClient.ZRevRank(ctx, key, id)
-	return cmd.Val() + 1, cmd.Err()
+func (r *RedisSorter) GetRank(ctx context.Context, key, member string) (int64, error) {
+	cmd := r.redisClient.ZRevRank(ctx, key, member)
+	if err := cmd.Err(); err != nil {
+		return 0, err
+	}
+	return cmd.Val() + 1, nil
 }
 
-func (r *RedisSorter) GetTop(ctx context.Context, key, id string) (string, error) {
-	cmd := r.redisClient.ZRevRange(ctx, key, 0, 1)
+func (r *RedisSorter) GetTop(ctx context.Context, key string) (string, error) {
+	cmd := r.redisClient.ZRevRange(ctx, key, 0, 0)
+	if err := cmd.Err(); err != nil {
+		return "", err
+	}
 	fmt.Println(cmd.Val())
-	return cmd.Val()[0], cmd.Err()
-}
-
-func (r *RedisSorter) GetHighTier(ctx context.Context, key, id string) (int64, error) {
-
-}
-
-func (r *RedisSorter) GetRankList(ctx context.Context, key, id string) (int64, error) {
-
+	return cmd.Val()[0], nil
 }
