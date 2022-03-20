446fe50cfd5f433cfa937b89a216fc9816d006be
kazdevl <39262724+kazdevl@users.noreply.github.com>
2022-03-21 00:04:05 +0900

    use ~ for generics

--- src/generics/basic/main.go
+++ src/generics/basic/main.go
@@ -6,6 +6,12 @@ import "fmt"
 type Number interface {
 	int64 | float64
 }
+type UnderlyingNumber interface {
+	~int64 | ~float64
+}
+
+type Sample int64
+type SSample Sample
 
 func main() {
 	// use Non-Generic functions
@@ -23,6 +29,21 @@ func main() {
 
 	// use Generic function
 	fmt.Printf("Generic Sums with Constraint: %v and %v\n", SumIntsOrFloats(ints), SumIntsOrFloats(floats))
+
+	// this code can't pass constraint
+	underlyingIntsVer1 := map[string]Sample{
+		"1": Sample(1),
+		"2": Sample(2),
+	}
+	underlyingIntsVer2 := map[string]SSample{
+		"1": SSample(1),
+		"2": SSample(2),
+	}
+	// fmt.Printf("Sample and SSample don'nt implement Number %v and %v\n", SumIntsOrFloats(underlyingIntsVer1), SumIntsOrFloats(underlyingIntsVer2))
+	fmt.Printf("Sample and SSample implement UnderlyingNumber %v and %v\n", SumUnderlyingIntsOrFloats(underlyingIntsVer1), SumUnderlyingIntsOrFloats(underlyingIntsVer2))
+
+	// use 1 type for 2 arguments
+	fmt.Print(SubIntsOrFloats[int64, float64](int64(10), 11.2))
 }
 
 // Non-Generic functions
@@ -49,4 +70,16 @@ func SumIntsOrFloats[K comparable, V Number](m map[K]V) V {
 		s += v
 	}
 	return s
+}
+
+func SumUnderlyingIntsOrFloats[K comparable, V UnderlyingNumber](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SubIntsOrFloats[A Number, B Number](a A, b B) A {
+	return A(int(a) - int(b))
 }
