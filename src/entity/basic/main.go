1fc16f3633152a9ce0638983b858af2df86b771f
kazu <39262724+kazdevl@users.noreply.github.com>
2022-04-17 18:20:58 +0900

    try to use ent

--- src/entity/basic/main.go
+++ src/entity/basic/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"app/entity/basic/ent"
+	"context"
+	"fmt"
+	"log"
+)
+
+func main() {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed: %v", err)
+	}
+}
+
+func CreateSample(ctx context.Context, client *ent.Client) (*ent.Sample, error) {
+	s, err := client.Sample.
+		Create().SetAge(30).SetName("sample").Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("sample was created: ", s)
+	return s, nil
+}
