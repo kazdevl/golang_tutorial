2c37cb894c7a1133896cfe81d3b33b108ae1b4b1
kazdevl <39262724+kazdevl@users.noreply.github.com>
2022-11-20 23:17:30 +0900

    mongodbの操作処理の追加

--- src/db/mongodb/main.go
+++ src/db/mongodb/main.go
@@ -6,10 +6,18 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type respData struct {
+	Id     primitive.ObjectID `json:"id" bson:"_id"`
+	Name   string             `json:"name" bson:"name"`
+	Value  float64            `json:"value" bson:"value"`
+	Sample string             `json:"sample" bson:"sample"`
+}
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
@@ -31,9 +39,26 @@ func main() {
 	fmt.Println("access mongodb")
 
 	collection := client.Database("testing").Collection("numbers")
+
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	res, err := collection.InsertOne(ctx, bson.D{{Key: "name", Value: "pi"}, {Key: "value", Value: 3.14159}})
-	id := res.InsertedID
-	fmt.Println(id)
+	docs := []interface{}{
+		bson.D{{Key: "name", Value: "pi"}, {Key: "value", Value: 3.14159}},
+		bson.D{{Key: "name", Value: "pi"}, {Key: "value", Value: 2 * 3.14159}},
+		bson.D{{Key: "name", Value: "pi"}, {Key: "sample", Value: "sample"}},
+		bson.D{{Key: "name", Value: "sample"}, {Key: "value", Value: 10}},
+	}
+	res, err := collection.InsertMany(ctx, docs)
+	fmt.Println(res)
+
+	filter := bson.D{{Key: "name", Value: "pi"}}
+	cur, err := collection.Find(context.TODO(), filter)
+	if err != nil {
+		panic(err)
+	}
+	for cur.Next(context.TODO()) {
+		var res respData
+		cur.Decode(&res)
+		fmt.Printf("res(find): %+v\n", res)
+	}
 }
