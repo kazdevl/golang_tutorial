86cf3703542e07092037ff40d3fd4693a1dd8ca9
kazdevl <39262724+kazdevl@users.noreply.github.com>
2022-11-20 22:57:06 +0900

    mongodbへのデータ登録

--- src/db/mongodb/main.go
+++ src/db/mongodb/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -28,4 +29,11 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("access mongodb")
+
+	collection := client.Database("testing").Collection("numbers")
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res, err := collection.InsertOne(ctx, bson.D{{Key: "name", Value: "pi"}, {Key: "value", Value: 3.14159}})
+	id := res.InsertedID
+	fmt.Println(id)
 }
