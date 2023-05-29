ad046f232e0501884be64c5ed359e60e5052b669
kazdevl <39262724+kazdevl@users.noreply.github.com>
2023-05-29 21:27:19 +0900

    atlasを試す

---
+++ src/migration/definition.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  -- `email` varchar(255) NOT NULL DEFAULT '',
+  `alter_add` varchar(255) NOT NULL DEFAULT '',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/migration/setup.sql
@@ -0,0 +1,3 @@
+-- sql
+-- insert data into user table
+INSERT INTO `user` (`username`, `password`, `email`) VALUES ('admin', 'admin', 'admin@localhost');
