6619208ac5dde99bbd6e6853bf2796ce039d0260
kazdevl <39262724+kazdevl@users.noreply.github.com>
2023-08-19 16:06:38 +0900

    cmpの確認

--- src/1.21_try/main.go
+++ src/1.21_try/main.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"cmp"
+	"fmt"
+)
 
 func main() {
 	// min・max
+	fmt.Println("min・maxの動作確認")
 	fmt.Println(min(1, 2, 3, 4, 5))
 	fmt.Println(max(11, 13, 14, 9))
 	fmt.Println(max("", "a", "c", "d"))
-	fmt.Print(min("", "a", "c", "d"))
+	fmt.Println(min("", "a", "c", "d"))
+
+	// cmp
+	fmt.Println("cmp.Compareの動作確認")
+	fmt.Println(cmp.Compare[float64](0.0, 1.2))
+	fmt.Println(cmp.Compare[float64](1.2, 0.0))
+	fmt.Println(cmp.Compare[float64](1.2, 1.2))
+
+	fmt.Println("cmp.Lessの動作確認")
+	fmt.Println(cmp.Less[float64](0.0, 1.2))
+	fmt.Println(cmp.Less[float64](1.2, 0.0))
+
 }
