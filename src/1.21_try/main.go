4e2ef6d7feb616181ce2d847d638fd38317abf05
kazdevl <39262724+kazdevl@users.noreply.github.com>
2023-08-19 16:19:39 +0900

    slicesパッケージを少し遊んだ

--- src/1.21_try/main.go
+++ src/1.21_try/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"cmp"
 	"fmt"
+	"slices"
 )
 
 func main() {
@@ -13,7 +14,7 @@ func main() {
 	fmt.Println(max("", "a", "c", "d"))
 	fmt.Println(min("", "a", "c", "d"))
 
-	// cmp
+	// cmpパッケージの動作確認
 	fmt.Println("cmp.Compareの動作確認")
 	fmt.Println(cmp.Compare[float64](0.0, 1.2))
 	fmt.Println(cmp.Compare[float64](1.2, 0.0))
@@ -23,4 +24,16 @@ func main() {
 	fmt.Println(cmp.Less[float64](0.0, 1.2))
 	fmt.Println(cmp.Less[float64](1.2, 0.0))
 
+	// slicesパッケージの動作確認
+	fmt.Println("slicesの動作確認")
+	ss := make([]int, 0, 10)
+	ss = append(ss, 1, 2, 3, 4, 5)
+	slices.Reverse(ss)
+	fmt.Println("reversed", ss)
+	ss = slices.Replace(ss, 1, 3, 10, 20)
+	fmt.Println("replaced", ss)
+	ss = slices.Delete(ss, 1, 3)
+	fmt.Println("deleted", ss)
+	ss = slices.Insert(ss, 1, 100, 200)
+	fmt.Println("inserted", ss)
 }
