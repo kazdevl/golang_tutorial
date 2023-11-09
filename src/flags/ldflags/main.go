49a0e5374875a30a529ef723c31d8d5be31a1447
kazdevl <39262724+kazdevl@users.noreply.github.com>
2023-11-10 00:26:38 +0900

    ldflagsを試した

--- src/flags/ldflags/main.go
+++ src/flags/ldflags/main.go
@@ -1,5 +1,12 @@
 package main
 
+/*
+go build -ldflags "\
+-X main.version=$(git describe --tag --abbrev=0) \
+-X main.revision=$(git rev-list -1 HEAD) \
+-X main.build=$(git describe --tags) \
+" flags/ldflags/main.go
+*/
 var (
 	version  string
 	revision string
