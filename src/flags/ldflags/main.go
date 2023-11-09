abf261040a35ab310b36a6135b338b94f724b466
kazdevl <39262724+kazdevl@users.noreply.github.com>
2023-11-10 00:15:37 +0900

    add file fot use ldflags

---
+++ src/flags/ldflags/main.go
@@ -0,0 +1,13 @@
+package main
+
+var (
+	version  string
+	revision string
+	build    string
+)
+
+func main() {
+	println("version:", version)
+	println("revision:", revision)
+	println("build:", build)
+}
