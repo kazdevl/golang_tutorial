43101efb38da383b5fef5b5a57b76fe9167e9c9a
kazdevl <39262724+kazdevl@users.noreply.github.com>
2024-07-15 22:35:06 +0900

    check context

--- src/context/pt/main.go
+++ src/context/pt/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 )
 
@@ -10,33 +9,7 @@ type Sample struct {
 }
 
 func main() {
-	// ctx := context.Background()
-	// defer func() {
-	// 	s := ctx.Value("sample").(*Sample)
-	// 	println(s.Name)
-	// }()
-
-	// s := &Sample{Name: "sample"}
-	// ctx = context.WithValue(context.Background(), "sample", s)
-	// nothing(ctx)
-
-	// checkPointer()
 	checkValue()
-}
-
-func nothing(_ context.Context) {
-}
-
-func checkPointer() {
-	v := &Sample{Name: "sample"}
-	fmt.Printf("before: %p\n", v)
-	defer func() {
-		fmt.Printf("defer_アドレス: %p\n", v)
-		fmt.Printf("defer_値: %+v\n", v)
-	}()
-
-	v.Name = "changed"
-	fmt.Printf("after: %p\n", v)
 }
 
 func checkValue() {
